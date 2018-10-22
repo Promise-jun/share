5291e340a2895139d7a8406e0ec70a6a40dececb
panjun <1022988217@qq.com>
2018-10-22 19:21:24 +0800

    20181021

--- js/expert_enter.js
+++ js/expert_enter.js
@@ -147,3 +147,104 @@ $("input[name='user-aptitude']").select({
         }
     ]
 });
+//打开图片示例
+var openPicExample = function(obj) {
+    var picSrc = $(obj).attr('data-src');
+    $('.pic_popup img').attr('src', picSrc);
+    $('.pic_popup').fadeIn(200);
+}
+//从业时间
+$("input[name='user-age-time']").datetimePicker({
+    title: "请选择您的从业时间",
+    toolbarCloseText: "确定",
+    times: function() {
+        return []
+    }
+});
+//受训背景
+$('.training-btn').click(function() {
+    $('.training-inp').fadeIn(200);
+    $('.training-inp input').focus();
+})
+$('.training-inp input').on('blur', function() {
+    var listDiv = document.createElement('div');
+    listDiv.className = 'weui-cell_swiped';
+
+    var topDiv = document.createElement('div');
+    topDiv.className = 'weui-cell__bd';
+    topDiv.style.transform = 'translate3d(0px, 0px, 0px)';
+    var content = document.createElement('div');
+    content.className = 'weui-cell';
+    content.innerHTML = $(this).val();
+    topDiv.appendChild(content);
+
+    var footDiv = document.createElement('div');
+    footDiv.className = 'weui-cell__ft';
+    var delA = document.createElement('a');
+    delA.className = 'weui-swiped-btn weui-swiped-btn_warn delete-swipeout';
+    delA.href = 'javascript:;';
+    delA.innerHTML = '删除';
+    delA.onclick = function() {
+        // this.appendNode.appendNode.remove();
+        this.parentNode.parentNode.remove();
+    }
+    footDiv.appendChild(delA);
+
+    listDiv.appendChild(topDiv);
+    listDiv.appendChild(footDiv);
+
+    document.getElementById('trainingList').appendChild(listDiv);
+    $('#trainingList>div').swipeout();
+    $('.training-inp').hide();
+    $(this).val('');
+})
+//多图片上传
+var uploads = function(c) {
+    "use strict";
+    var $c = document.querySelector(c),
+        files = $c.files;
+    $.each(files, function(index, item) {
+        var reader = new FileReader();
+        reader.readAsDataURL(item);
+        reader.onload = function(e) {
+            var li = document.createElement('li');
+            li.className = 'weui-uploader__file';
+            li.style.backgroundImage = 'url(' + e.target.result + ')';
+            var aDel = document.createElement('a');
+            aDel.href = 'javascript:;';
+            aDel.onclick = function() {
+                this.parentNode.remove();
+            }
+            li.appendChild(aDel);
+            $c.parentNode.previousElementSibling.appendChild(li);
+        };
+    })  
+}
+//打开文字示例
+var openPicExample = function(name) {
+    var textCont = document.getElementById('textCont');
+    textCont.innerHTML = '';
+    if (name === 'specialty') { //专业背景
+        var p1 = document.createElement('p');
+        p1.innerHTML = '国家心理咨询师培训师（认证师资）；';
+        var p2 = document.createElement('p');
+        p2.innerHTML = '企业EAP咨询师；';
+        var p3 = document.createElement('p');
+        p3.innerHTML = '中国心理卫生协会会员；';
+        var p4 = document.createElement('p');
+        p4.innerHTML = '中科院心理所医学心理学硕士研究生；';
+        textCont.appendChild(p1);
+        textCont.appendChild(p2);
+        textCont.appendChild(p3);
+        textCont.appendChild(p4);
+    } else if (name === 'scly') { //擅长领域
+        var p1 = document.createElement('p');
+        p1.innerHTML = '婚恋情感，家庭矛盾，个人成长，亲子教育，人际职场，情绪压力，性心理；按照擅长程度来排列，排在前面的会优先派单';
+        textCont.appendChild(p1);
+    } else if (name === 'zxfg') { //咨询风格
+        var p1 = document.createElement('p');
+        p1.innerHTML = '耐心、和蔼、严谨、敏锐、热诚，用几个关键词来形容自己的风格';
+        textCont.appendChild(p1);
+    }
+    $('.text_popup').fadeIn(200);
+}
